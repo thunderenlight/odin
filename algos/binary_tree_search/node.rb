0b72bc58813d48b4448f92a660cc8b278fd25036
thunderenlight <thunderenlight@gmail.com>
2016-03-16 13:05:02 -0500

    Adding algorithm breadth first

--- algos/binary_tree_search/node.rb
+++ algos/binary_tree_search/node.rb
@@ -1,12 +1,12 @@
 class Tree
 
 		Node = Struct.new(:value, :left, :right)
-		attr_accessor :value, :parent, :child, :left, :right
+		attr_accessor :root_node, :value, :parent, :child, :left, :right
 
-	def initialize(root_node = nil )
+	def initialize(root_value = nil )
 		@parent ||= parent
 		@child ||= child
-		@root_node = Node.new(root_node, nil, nil) unless root_node.nil?
+		@root_node = Node.new(root_value, nil, nil) unless root_value.nil?
 
 	end
 
@@ -25,20 +25,37 @@ class Tree
 		# branch
 		new_node = Node.new(value, nil, nil)
 		@root_node = new_node and return if @root_node.nil?
-		
+		build_branch(@root_node, new_node)
 
 	end
 
-	# def build_sort_tree(unsorted_array)
-	# 	# iterate through array and if num is smaller put in left array
-	# 	#  node for each of the possible cases(size place parent no of children)
-	# 	# if item < item +1 left << item else right << item
-	# 	# if item < left_array[1] << left_array[1][1]
-	# 	# tree.values.min.min ==val ? tree.values.min.max ==? then tree.values.min.max = v
-	# 	# if !left self.left = Node.new(v)
+	def build_branch(tree_node, new_node)
+		# iterate through array and if num is smaller put in left array
+		#  node for each of the possible cases(size place parent no of children)
+		# if item < item +1 left << item else right << item
+		# if item < left_array[1] << left_array[1][1]
+		# tree.values.min.min ==val ? tree.values.min.max ==? then tree.values.min.max = v
+		# if !left self.left = Node.new(v)
+		tree_node = new_node and return if tree_node.nil?
+		return tree_node if tree_node.value == new_node.value
 
-	# end
+		if new_node.value < tree_node.value
+			return build_branch(tree_node.left, new_node) unless tree_node.left.nil?
+			return tree_node.left = new_node
+		end
 
+		return build_branch(tree_node.right, new_node) unless tree_node.right.nil?
+			return tree_node.right = new_node
+	end
+	def search(value, type_of_search)
+		return nil if @root_node.nil?
+		if type_of_search =	"breadth"
+			breadth_first_search(value)
+		else
+			depth_first_search(value)
+		end
+	end
+	
 	def breadth_first_search(target_value)
 		#  
 		# iterate tree keys and if key > val go to next child if child has child if not smaller go to 
